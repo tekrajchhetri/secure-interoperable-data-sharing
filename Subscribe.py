72ac2015f4f35f0c30788d963db2f22169bb98be
Tek Raj Chhetri <tekrajchhetri@gmail.com>
2022-05-08 14:29:31 +0200

    subscribe + initiate tranform step

---
+++ Subscribe.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# @Time    : 08.05.22 11:22
+# @Author  : Tek Raj Chhetri
+# @Email   : tekraj.chhetri@sti2.at
+# @Web     : https://tekrajchhetri.com/
+# @File    : Subscribe.py
+# @Software: PyCharm
+from connection_manager import ConnectionManager
+from data_transformation_engine import DataTransformationEngine
+class Subscribe(ConnectionManager):
+    def callback(self, ch, method, properties, body):
+        dte = DataTransformationEngine()
+        dte.transform(body)
+
+    def subscribe(self):
+        connection = self.rabbit_connection()
+        channel = connection.channel()
+        channel.basic_consume(
+            queue=self.get_rabbit_config_details()["queuename"],
+            on_message_callback=self.callback,
+            auto_ack=True)
+        channel.start_consuming()
+
+
+
+if __name__ == '__main__':
+    s = Subscribe()
+    s.subscribe()
