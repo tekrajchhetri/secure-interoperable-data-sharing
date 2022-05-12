d9026315f268e48b82f8109e5cf5e5007a871872
Tek Raj Chhetri <tekrajchhetri@gmail.com>
2022-05-12 13:45:35 +0200

    listener added

---
+++ subscribe.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Time    : 12.05.22 13:40
+# @Author  : Tek Raj Chhetri
+# @Email   : tekraj.chhetri@sti2.at
+# @Web     : https://tekrajchhetri.com/
+# @File    : subscribe.py
+# @Software: PyCharm
+
+from subscriber.Subscribe import Subscribe
+if __name__ == '__main__':
+    s = Subscribe()
+    s.subscribe()
