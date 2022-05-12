f8715c06eb04eb4c4bc9b10bfc7ce486b68a9fce
Tek Raj Chhetri <tekrajchhetri@gmail.com>
2022-05-12 13:30:03 +0200

    pkg created

---
+++ pipeline/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Time    : 12.05.22 13:25
+# @Author  : Tek Raj Chhetri
+# @Email   : tekraj.chhetri@sti2.at
+# @Web     : https://tekrajchhetri.com/
+# @File    : __init__.py.py
+# @Software: PyCharm
