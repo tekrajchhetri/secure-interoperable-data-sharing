cb6f486784deafec4a035bf224ad7c8578b1c9a4
Tek Raj Chhetri <tekrajchhetri@gmail.com>
2022-05-12 13:30:41 +0200

    starter code

---
+++ start.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# @Time    : 12.05.22 13:26
+# @Author  : Tek Raj Chhetri
+# @Email   : tekraj.chhetri@sti2.at
+# @Web     : https://tekrajchhetri.com/
+# @File    : start.py
+# @Software: PyCharm
+
+from subscriber.Subscribe import Subscribe
+
+if __name__ == '__main__':
+    s = Subscribe()
+    s.subscribe()
+
