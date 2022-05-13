a3286e24600682062464f4864731231f17678e0b
Tek Raj Chhetri <tekrajchhetri@gmail.com>
2022-05-13 22:08:45 +0200

    transform rfdlib generated graph for insertion

--- data_transformation/data_transformation_engine.py
+++ data_transformation/data_transformation_engine.py
@@ -6,37 +6,11 @@
 # @File    : data_transformation_engine.py
 # @Software: PyCharm
 import sys
-
-
-import textwrap
+from rdflib import Graph
 class DataTransformationEngine:
-
-    def get_unit(self, data):
-        if "temperature" in data['observedproperty']:
-            return "om:degreeCelsius"
-        elif "humidity" in data['observedproperty']:
-            return "om:percent"
-    def transform(self, data):
-        if type(data) != dict:
-            raise TypeError("Data should be of type dictionary")
-        transformed_data = textwrap.dedent("""
-                :observation\/{4} a sosa:Observation;
-                sosa:hasSimpleResult :{1} ;
-                sosa:madeBySensor :sensor\/{0} ;
-                sosa:resultTime :{2};
-                :hasHash :{5};
-                om:hasUnit :{6};
-                sosa:observedProperty :{3} .
-                :sensor\/{0} a sosa:Sensor ;
-                sosa:observes :observation\/{4} .
-        """).format(
-                    data["observationsensorid"],
-                    data["observationresult"],
-                    data["resultobservationtime"],
-                    data["observedproperty"],
-                    data["observationid"],
-                    data["hashvalue"],
-                    self.get_unit(data=data)
-                   )
+    def transform(self, rdf_data):
+        transformed_data = ""
+        for subject, predicate, obj in rdf_data:
+            transformed_data = transformed_data + subject.n3() + " " + predicate.n3() + " " + obj.n3() + " . \n"
 
         return transformed_data
