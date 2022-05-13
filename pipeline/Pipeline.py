dfabe4252e02360c5552bad4e56f2db94ef389e9
Tek Raj Chhetri <tekrajchhetri@gmail.com>
2022-05-13 12:42:28 +0200

    updated for analytics

--- pipeline/Pipeline.py
+++ pipeline/Pipeline.py
@@ -9,21 +9,24 @@ from validation.validation_engine import ValidationEngine
 import json
 from data_migrator.data_migration import DataMigration
 from data_transformation.data_transformation_engine import DataTransformationEngine
+from analytics.edge_intelligence import EdgeIntelligence
+from helper.helper import Helpers
 class Pipeline:
 
-    def init_pipeline(self, data):
+    def validate_transform_migrate(self, data):
         data = json.loads(data)
         if(ValidationEngine().validate(data)):
             transformed_data = DataTransformationEngine().transform(data=data)
             status = DataMigration().migrate_to_gdb(transformed_data)
-            print(data)
             return status
 
-
-        # call legal engine
-        # perform transformation
-        # perform validation
-        # perform analytics
-        # migrate
-
+    def validate_apply_intelligence(self, data):
+        data = json.loads(data)
+        mode = Helpers().edge_intelligence_mode()["intelligence_mode"]
+        if (ValidationEngine().validate(data)):
+            if mode=="edge":
+                res =  EdgeIntelligence().start_edge_intelligence(data, mode)
+                print(res)
+            else:
+                EdgeIntelligence().start_edge_intelligence(data, mode)
 
