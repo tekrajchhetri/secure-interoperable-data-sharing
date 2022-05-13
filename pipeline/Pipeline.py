e2c54c57acbd89d7449e5af1e4cbdd54823da5af
Tek Raj Chhetri <tekrajchhetri@gmail.com>
2022-05-13 12:56:46 +0200

    consent check added

--- pipeline/Pipeline.py
+++ pipeline/Pipeline.py
@@ -10,23 +10,32 @@ import json
 from data_migrator.data_migration import DataMigration
 from data_transformation.data_transformation_engine import DataTransformationEngine
 from analytics.edge_intelligence import EdgeIntelligence
+from legal.legal_engine import LegalEngine
 from helper.helper import Helpers
 class Pipeline:
 
+
     def validate_transform_migrate(self, data):
         data = json.loads(data)
-        if(ValidationEngine().validate(data)):
-            transformed_data = DataTransformationEngine().transform(data=data)
-            status = DataMigration().migrate_to_gdb(transformed_data)
-            return status
+        if LegalEngine().hasConsent(data=data):
+            if(ValidationEngine().validate(data)):
+                transformed_data = DataTransformationEngine().transform(data=data)
+                status = DataMigration().migrate_to_gdb(transformed_data)
+                return status
+        else:
+            print({"message":"No consent for operations"})
 
     def validate_apply_intelligence(self, data):
         data = json.loads(data)
-        mode = Helpers().edge_intelligence_mode()["intelligence_mode"]
-        if (ValidationEngine().validate(data)):
-            if mode=="edge":
-                res =  EdgeIntelligence().start_edge_intelligence(data, mode)
-                print(res)
-            else:
-                EdgeIntelligence().start_edge_intelligence(data, mode)
+        if LegalEngine().hasConsent(data=data):
+            mode = Helpers().edge_intelligence_mode()["intelligence_mode"]
+            if (ValidationEngine().validate(data)):
+                if mode == "edge":
+                    res = EdgeIntelligence().start_edge_intelligence(data, mode)
+                    print(res)
+                else:
+                    EdgeIntelligence().start_edge_intelligence(data, mode)
+        else:
+            print({"message":"No consent for operations"})
+
 
